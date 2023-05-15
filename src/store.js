e4ce4f352c90c21aa4e4b4e94fb1675811ee0256
Jaewoong Hwang <noa9925@gmail.com>
2023-05-15 21:49:15 +0900

    20230515
    1. 메인 페이지 전체적인 레이아웃 구현
    - 식재료 추가 목록 (SelectedList)
    - 옵션 (Option)
    
    2. 옵션 직접 입력하여 추가 구현

--- src/store.js
+++ src/store.js
@@ -18,16 +18,32 @@ let select = createSlice({
   },
 });
 
-export let { setSelectFalse, setSelectTrue } = select.actions;
-
 let selected = createSlice({
   name: "selected",
-  initialState: ["대파", "양파"],
+  initialState: ["대파", "양파", "저민 돼지고기", "대파"],
+});
+
+let option = createSlice({
+  name: "option",
+  initialState: ["저염식", "최고비건식", "주펄찜", "손님들 접대용 파티음식", "치팅데이"],
+});
+
+let inputClick = createSlice({
+  name: "inputClick",
+  initialState: true,
+  reducers: {
+    setToggle: (state) => !state,
+  },
 });
 
 export default configureStore({
   reducer: {
     select: select.reducer,
     selected: selected.reducer,
+    option: option.reducer,
+    inputClick: inputClick.reducer,
   },
 });
+
+export let { setSelectFalse, setSelectTrue } = select.actions;
+export let { setToggle } = inputClick.actions;
