c4992acd0e8e29af8eeb67e89817c68b18082207
hyeond0 <a98792530@gmail.com>
2023-05-16 13:56:52 +0900

    openAI API KEY 환경변수 설정

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
+
 const path = require("path");
 const app = express();
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: messages,
+});
+
 // react와 nodejs 서버간 ajax 요청, cors 처리
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 var cors = require("cors");
 app.use(cors());
 
