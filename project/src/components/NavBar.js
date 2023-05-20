4b5602e284ffb3d6808c4d5934670ec7b46b2469
Jaewoong Hwang <noa9925@gmail.com>
2023-05-21 00:58:01 +0900

    20230521 (2)
    Styled-components Refactoring

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,21 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import logoBM from "../img/logoBM.png";
+import styled from "styled-components";
 
 function NavBar() {
   let Navigate = useNavigate();
+  const NavBar = styled.div`
+    width: 100vw;
+    height: 100px;
+    background-color: #f2f0ef;
+    padding: 0px 4%;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
 
   return (
-    <div className="navBar">
+    <NavBar>
       <p style={{ verticalAlign: "center", minWidth: "30px" }}>Left</p>
       <img
-        className="logoImg"
+        style={{ height: "40px" }}
         src={logoBM}
         onClick={() => {
           Navigate("/");
         }}
       ></img>
       <p style={{ verticalAlign: "center", minWidth: "30px" }}>right</p>
-    </div>
+    </NavBar>
   );
 }
 
--- project/src/components/SelectList.js
+++ project/src/components/SelectList.js
@@ -1,20 +1,44 @@
 import { useDispatch } from "react-redux";
 import { pushSelected, removeSelected } from "../store.js";
+import styled from "styled-components";
 
 function SelectList(props) {
   let dispatch = useDispatch();
 
+  const SelectContainer = styled.div`
+    width: 100%;
+    padding: 10px 0px;
+
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    gap: 10px;
+
+    border-radius: 75px;
+  `;
+
+  const SelectItem = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: #ffffff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 0;
+    flex-shrink: 0;
+
+    border-radius: 50%;
+    box-shadow: 0px 10px 20px -5px rgba(153, 153, 153, 0.2);
+  `;
+
   return (
     <>
-      <container className="selectContainer">
+      <SelectContainer>
         {props.select.map(function (item, i) {
           return (
             <>
-              {/* 20230520 */}
-              {/* 아이템 클릭했을 때, Selected List에 해당 아이템 추가 */}
-              {/* 이미 들어가있는 상태였다면, 해당 아이템을 찾아 제거 */}
-              <div
-                className="selectItem"
+              <SelectItem
                 onClick={(e) => {
                   const selectedValue = e.currentTarget.innerText;
 
@@ -26,11 +50,11 @@ function SelectList(props) {
                 }}
               >
                 {item.type}
-              </div>
+              </SelectItem>
             </>
           );
         })}
-      </container>
+      </SelectContainer>
     </>
   );
 }
