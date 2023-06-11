acc64571338722c73603c31b0b1d1f7db231fdf3
hyeond0 <a98792530@gmail.com>
2023-06-11 16:49:24 +0900

    deploy 폴더 나누기

--- init.js
+++
@@ -1,8 +0,0 @@
-import app from "./server";
-
-const PORT = process.env.PORT || 8080;
-
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
