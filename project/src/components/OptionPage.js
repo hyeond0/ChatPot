c88db29530c1172aaf120ce67a0255bceb8418b2
Jaewoong Hwang <noa9925@gmail.com>
2023-06-28 10:07:34 +0900

    20230628

--- project/src/components/OptionPage.js
+++ project/src/components/OptionPage.js
@@ -27,6 +27,7 @@ function OptionTest(props) {
 
   const [isEmpty, setEmpty] = useState(true);
   const [emptyAlert, setEmptyAlert] = useState(false);
+  const [targetValue, setTargetValue] = useState("");
   const [isWrongAlert, setWrongAlert] = useState(false);
 
   useEffect(() => {
@@ -103,13 +104,32 @@ function OptionTest(props) {
   function handleOptionClick(e) {
     e.preventDefault();
     const selectedValue = e.currentTarget.innerText;
-    console.log(selectedValue);
     if (State.selectedOption.includes(selectedValue)) {
       dispatch(removeOption(selectedValue));
     } else {
       dispatch(pushOption(selectedValue));
     }
+    setTargetValue(selectedValue);
   }
+
+  const FirstDiv = styled.div`
+    height: 160px;
+  `;
+
+  const SecondDiv = styled.div`
+    width: 100%;
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    overflow-y: auto;
+  `;
+
+  const ThirdDiv = styled.div`
+    height: 160px;
+  `;
 
   return (
     <>
@@ -118,7 +138,16 @@ function OptionTest(props) {
       <SContainer>
         <Title>2. 옵션을 선택하세요</Title>
         <Context>선택을 원하지 않는다면, {viewportWidth < 768 && <br />}바로 제작 버튼을 눌러도 좋아요</Context>
-        <SRow style={{ maxHeight: "70%", paddingBottom: "10px" }}>
+        <button
+          style={{ display: "none" }}
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(pushOption("옵션옵션"));
+          }}
+        >
+          추가
+        </button>
+        <SRow style={{ marginBottom: "180px" }}>
           {State.option.map(function (item, i) {
             return (
               <>
@@ -133,58 +162,22 @@ function OptionTest(props) {
             );
           })}
         </SRow>
-        <SRow>
-          <OptionWrite
-            onSubmit={handleSubmit((data) => {
-              if (data.option.length) {
-                dispatch(AddOption(data.option));
-                reset();
-              }
-            })}
-          >
-            <CustomInput type="text" placeholder="옵션 직접 추가" {...register("option")} />
-            <BtnSubmit type="submit">
-              <StyledBiListPlus />
-            </BtnSubmit>
-          </OptionWrite>
-        </SRow>
       </SContainer>
 
-      {isWrongAlert ? (
-        <>
-          <AlertBg>
-            <AlertContainer>
-              <AlertDiv>
-                <Lottie
-                  style={{ width: "70%" }}
-                  options={errorLottie}
-                  height={300}
-                  isPaused={false}
-                  isStopped={false}
-                  isClickToPauseDisabled={true}
-                />
-              </AlertDiv>
-              <AlertDiv>
-                <h3>
-                  문제가 발생했습니다.
-                  <br />
-                  잠시 후 다시 시도해주세요.
-                </h3>
-              </AlertDiv>
-              <AlertDiv>
-                <BsFillXCircleFill
-                  onClick={() => setEmptyAlert(false)}
-                  style={{ fontSize: "30px", color: "lightgray", cursor: "pointer" }}
-                />
-              </AlertDiv>
-            </AlertContainer>
-          </AlertBg>
-        </>
-      ) : (
-        <></>
-      )}
-
       <Footer>
+        <OptionWrite
+          onSubmit={handleSubmit((data) => {
+            if (data.option.length) {
+              dispatch(AddOption(data.option));
+              reset();
+            }
+          })}
+        >
+          <CustomInput type="text" placeholder="옵션 직접 추가" {...register("option")} />
+          <BtnSubmit type="submit">
+            <StyledBiListPlus />
+          </BtnSubmit>
+        </OptionWrite>
         <ButtonNavigate
           onClick={() => {
             Navigate("/selectIngredients", { state: { direction: "left" } });
@@ -226,36 +219,6 @@ function OptionTest(props) {
           </MakeBtn>
         )}
 
-        {emptyAlert ? (
-          <>
-            <AlertBg>
-              <AlertContainer>
-                <AlertDiv>
-                  <Lottie
-                    style={{ width: "70%" }}
-                    options={EmptyAnimation}
-                    height={300}
-                    isPaused={false}
-                    isStopped={false}
-                    isClickToPauseDisabled={true}
-                  />
-                </AlertDiv>
-                <AlertDiv>
-                  <h3>식재료를 선택해주세요!</h3>
-                </AlertDiv>
-                <AlertDiv>
-                  <BsFillXCircleFill
-                    onClick={() => setEmptyAlert(false)}
-                    style={{ fontSize: "30px", color: "lightgray", cursor: "pointer" }}
-                  />
-                </AlertDiv>
-              </AlertContainer>
-            </AlertBg>
-          </>
-        ) : (
-          <></>
-        )}
-
         <ButtonNavigate
           onClick={() => {
             dispatch(initOption());
@@ -265,6 +228,70 @@ function OptionTest(props) {
           <BsArrowRepeat style={{ fontSize: "25px", color: "#f2f0ef" }}></BsArrowRepeat>
         </ButtonNavigate>
       </Footer>
+
+      {isWrongAlert ? (
+        <>
+          <AlertBg>
+            <AlertContainer>
+              <AlertDiv>
+                <Lottie
+                  style={{ width: "70%" }}
+                  options={errorLottie}
+                  height={300}
+                  isPaused={false}
+                  isStopped={false}
+                  isClickToPauseDisabled={true}
+                />
+              </AlertDiv>
+              <AlertDiv>
+                <h3>
+                  문제가 발생했습니다.
+                  <br />
+                  잠시 후 다시 시도해주세요.
+                </h3>
+              </AlertDiv>
+              <AlertDiv>
+                <BsFillXCircleFill
+                  onClick={() => setEmptyAlert(false)}
+                  style={{ fontSize: "30px", color: "lightgray", cursor: "pointer" }}
+                />
+              </AlertDiv>
+            </AlertContainer>
+          </AlertBg>
+        </>
+      ) : (
+        <></>
+      )}
+
+      {emptyAlert ? (
+        <>
+          <AlertBg>
+            <AlertContainer>
+              <AlertDiv>
+                <Lottie
+                  style={{ width: "70%" }}
+                  options={EmptyAnimation}
+                  height={300}
+                  isPaused={false}
+                  isStopped={false}
+                  isClickToPauseDisabled={true}
+                />
+              </AlertDiv>
+              <AlertDiv>
+                <h3>식재료를 선택해주세요!</h3>
+              </AlertDiv>
+              <AlertDiv>
+                <BsFillXCircleFill
+                  onClick={() => setEmptyAlert(false)}
+                  style={{ fontSize: "30px", color: "lightgray", cursor: "pointer" }}
+                />
+              </AlertDiv>
+            </AlertContainer>
+          </AlertBg>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
@@ -315,7 +342,7 @@ const GlobalStyle = createGlobalStyle`
 
 const Title = styled.div`
   font-size: 150%;
-  margin: 20px 0px;
+  margin: 20px 0px 0px 0px;
   font-weight: 600;
 
   display: flex;
@@ -324,25 +351,26 @@ const Title = styled.div`
 
   @media (min-width: 768px) {
     font-size: 180%;
+    margin: 0px 0px 0px 0px;
   }
 `;
 
 const Context = styled.div`
   font-size: 90%;
-  margin: 20px 0px;
+  margin: 10px 0px 20px 0px;
 `;
 
 const SContainer = styled(Container)`
   width: 100%;
-  height: 80%;
+  height: 100%;
   margin: 0px 0px 0px 0px;
   padding: 0px 30px;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   overflow-y: none;
-
-  @media (min-width: 768px) {
-    padding: 0px 30px;
-  }
 `;
 
 const SRow = styled(Row)`
@@ -350,16 +378,10 @@ const SRow = styled(Row)`
   margin: 0px 0px 0px 0px;
   padding: 0px 0px 0px 0px;
 
-  overflow-y: auto;
   overflow-x: hidden;
+  overflow-y: auto;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: start;
-
-  @media (min-width: 768px) {
-    gap: 5px;
-  }
+  gap: 12px;
 `;
 
 const SCol = styled(Col)`
@@ -375,8 +397,8 @@ const OptionList = styled.div`
   min-width: 100px;
   width: 100%;
   height: 80px;
-  /* padding: 30px 20px; */
-  margin: 4px 0px;
+  padding: 30px 20px;
+  /* margin: 4px 0px; */
   text-align: start;
   background-color: white;
 
@@ -388,9 +410,9 @@ const OptionList = styled.div`
 
   overflow-y: hidden;
   overflow-x: auto;
-  box-shadow: 0px 10px 20px -5px rgba(153, 153, 153, 0.2);
+  box-shadow: 0px 10px 3px -6px rgba(29, 18, 10, 0.05);
 
-  transition: transform 0.3s ease;
+  transition: all 0.5s ease;
   cursor: pointer;
 
   @media (min-width: 768px) {
@@ -399,8 +421,6 @@ const OptionList = styled.div`
     &:hover {
       color: #f2f0ef;
       background-color: #352e29;
-
-      /* transition: background-color 1.5s; */
     }
   }
 
@@ -409,11 +429,13 @@ const OptionList = styled.div`
     `
   color: #f2f0ef;
   background-color: #352e29;
+
+  // box-shadow: none;
 `}
 `;
 
 const OptionWrite = styled.form`
-  width: 70%;
+  width: 80%;
   height: 30px;
   padding: 30px 30px;
   text-align: start;
@@ -425,8 +447,7 @@ const OptionWrite = styled.form`
   align-items: center;
 
   box-shadow: 0px 10px 20px -5px rgba(29, 18, 10, 0.317);
-
-  border-radius: 100px;
+  border-radius: 10px;
 
   position: fixed;
   bottom: 130px;
@@ -436,7 +457,7 @@ const OptionWrite = styled.form`
 
   @media (min-width: 768px) {
     width: 60%;
-    bottom: 160px;
+    /* bottom: 160px; */
   }
 `;
 
@@ -476,7 +497,7 @@ const Footer = styled.div`
 
   gap: 2%;
   position: fixed;
-  bottom: 50px;
+  bottom: 40px;
   margin: 0 auto;
   left: 0;
   right: 0;
@@ -506,7 +527,7 @@ const ButtonNavigate = styled.div`
 `;
 
 const MakeBtn = styled.div`
-  width: 50%;
+  width: 60%;
   height: 60px;
   color: #f2f0ef;
   background-color: #352e29;
@@ -516,7 +537,7 @@ const MakeBtn = styled.div`
   align-items: center;
 
   border-radius: 200px;
-  box-shadow: 0px 10px 20px -5px rgba(153, 153, 153, 0.5);
+  box-shadow: 0px 10px 20px -5px rgba(29, 18, 10, 0.317);
 
   cursor: pointer;
   transition: transform 0.3s ease;
@@ -524,6 +545,17 @@ const MakeBtn = styled.div`
   &:hover {
     transform: scale(1.05);
   }
+
+  /* ${({ isEmpty }) =>
+    !isEmpty &&
+    `
+  cursor:default;
+  opacity:0.4;
+
+  &:hover {
+    transform: none;
+  }
+`} */
 `;
 
 const Loading = styled.div`
@@ -579,7 +611,7 @@ const AlertContainer = styled.div`
   flex-direction: column;
 
   border-radius: 20px;
-  box-shadow: 0px 10px 20px -5px rgba(153, 153, 153, 0.5);
+  box-shadow: 0px 10px 20px -5px rgba(29, 18, 10, 0.317);
 
   @media (min-width: 768px) {
   }
