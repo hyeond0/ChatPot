22aab87a86b6ab55134628e703f3125d83a8955b
hyeond0 <a98792530@gmail.com>
2024-08-02 23:05:58 +0900

    [feat] CORS 추가

--- backend/src/main/java/kr/co/chatpot/controller/RecipeController.java
+++ backend/src/main/java/kr/co/chatpot/controller/RecipeController.java
@@ -5,6 +5,7 @@ import kr.co.chatpot.dto.request.OptionRequest;
 import kr.co.chatpot.dto.request.ReRecipeRequest;
 import kr.co.chatpot.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecipeController {
     private final RecipeService recipeService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/selectOption")
     public RecipeDto selectOption(@RequestBody OptionRequest request) {
         return recipeService.recommendRecipe(request);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/recipe")
     public RecipeDto recipe(@RequestBody ReRecipeRequest request) {
         return recipeService.retryRecommend(request);
